80e2de1fcdb53f90de36d245dd652391de401e80
Vinicius Duarte <viniciusrduarte8@gmail.com>
2022-05-05 21:11:36 -0300

    dswa10

--- consulta.js
+++ consulta.js
@@ -1,30 +1,30 @@
-const findDocuments = function(db, callback) {
-    // Get the documents collection
-    const collection = db.collection('contatos');
-    // Find some documents
-    collection.find({}).toArray(function(err, docs) {
-        assert.equal(err, null);
-        console.log("Foram encontrados os seguintes docs:");
-        console.log(docs)
-        callback(docs);
-    });
-};
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const { MongoClient } = require('mongodb');
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+
 // Connection URL
-const url = 'mongodb+srv://user:password@clusterdswa5.x96ld.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const url = 'mongodb://dswa5:dswa5@cluster0-shard-00-00.aoz22.mongodb.net:27017,cluster0-shard-00-01.aoz22.mongodb.net:27017,cluster0-shard-00-02.aoz22.mongodb.net:27017/ifsp?ssl=true&replicaSet=atlas-12vniy-shard-0&authSource=admin&retryWrites=true&w=majority';
+const client = new MongoClient(url);
+
 // Database Name
 const dbName = 'ifsp';
-// Use connect method to connect to the server
-MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, client) {
-    assert.equal(null, err);
-    console.log("Aluno: Fabio Teixeira");
-    console.log("Servidor conectado!");
 
-    const db = client.db(dbName);
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Aluno: Vinicius Rodrigues Duarte');
+  const db = client.db(dbName);
+  const collection1 = db.collection('contatos');
+  const collection2 = db.collection('cursos');
+  const findResult1 = await collection1.find({}).toArray();
+  console.log('Found documents =>', findResult1);
+  // the following code examples can be pasted here...
+  const findResult2 = await collection2.find({}).toArray();
+  console.log('Found documents =>', findResult2);
+    return 'fim.';
+}
 
-    findDocuments(db, function() {
-        client.close();
-    });
-
-});
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var http = require('http');
 var app = require('./config/express')();
-http.createServer(app).listen(app.get('port'), function(){
-	console.log('Express Server escutando na porta ' + app.get('port'));
+const url = 'mongodb://dswa5:dswa5@cluster0-shard-00-00.aoz22.mongodb.net:27017,cluster0-shard-00-01.aoz22.mongodb.net:27017,cluster0-shard-00-02.aoz22.mongodb.net:27017/ifsp?ssl=true&replicaSet=atlas-12vniy-shard-0&authSource=admin&retryWrites=true&w=majority';
+require('./config/database.js')(url);
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express Server escutando na porta ' + app.get('port'));
 });
